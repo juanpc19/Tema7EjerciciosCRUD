58c7068daf1fce4480efe750341c2fbe29cc1063
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-04-07 12:35:09 +0200

    ej1 hecho falta comentar

--- Tema7EjerciciosCRUD/src/ejercicio1/Alumno.java
+++ Tema7EjerciciosCRUD/src/ejercicio1/Alumno.java
@@ -2,4 +2,42 @@ package ejercicio1;
 
 public class Alumno {
 
+	private String nombre = "";
+
+	private double notaMedia;
+
+	public Alumno() {
+		super();
+		nombre = null;
+	}
+
+	public Alumno(String nombre, double notaMedia) {
+		super();
+		this.nombre = nombre;
+		this.notaMedia = notaMedia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+
+	public void setNotaMedia(double notaMedia) {
+		if (notaMedia > 0.0) {
+			this.notaMedia = notaMedia;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno: " + nombre + ", Nota media: " + notaMedia;
+	}
+
 }
--- Tema7EjerciciosCRUD/src/ejercicio1/Main.java
+++ Tema7EjerciciosCRUD/src/ejercicio1/Main.java
@@ -1,9 +1,117 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner dogma = new Scanner(System.in);
+
+		int seleccionUsuario = 0;
+
+		String nombre = "";
+
+		double notaMedia;
+
+		Alumno alumnos[] = new Alumno[30];
+
+		// bucle que creara 30 variables tipo Alumno con el contructor por defecto y las
+		// ira guardando en las posiciones del array alumnos
+		for (int i = 0; i < alumnos.length; i++) {
+			alumnos[i] = new Alumno();
+		}
+
+		do {
+			System.out.println(
+					"ALUMNOS/AS \n--------------------\n--------------------\n1.Listado.\n2.Nuevo Alumno.\n3.Modificar.\n4.Borrar.\n5.Salir");
+
+			seleccionUsuario = dogma.nextInt();
+			dogma.nextLine();
+
+			switch (seleccionUsuario) {
+
+			case 1 -> {
+				for (int i = 0; i < alumnos.length; i++) {
+
+					if (alumnos[i].getNombre() != null) {
+						System.out.println(alumnos[i].toString());
+
+					}
+				}
+			}
+
+			case 2 -> {
+
+				System.out.println("Introduzca nombre y apellidos del alumno: ");
+				nombre = dogma.nextLine();
+
+				System.out.println("Introduzca nota media del alumno: ");
+				notaMedia = dogma.nextDouble();
+				if (notaMedia < 0.0) {
+					System.out.println(
+							"Ha introducido una nota negativa que no se asignara al alumno, repita el proceso");
+				} else {
+
+					for (int i = 0; i < alumnos.length; i++) {
+
+						if (alumnos[i].getNombre() == null) {
+							alumnos[i].setNombre(nombre);
+							alumnos[i].setNotaMedia(notaMedia);
+							break;
+						}
+					}
+				}
+			}
+
+			case 3 -> {
+				System.out.println("Introduzca nombre y apellidos del alumno cuya nota desea modificar: ");
+				nombre = dogma.nextLine();
+
+				for (int i = 0; i < alumnos.length; i++) {
+
+					if (alumnos[i].getNombre().equals(nombre)) {
+						System.out.println("Introduzca nuevo valor para la nota media a modificar: ");
+						notaMedia = dogma.nextDouble();
+						if (notaMedia < 0.0) {
+							System.out.println(
+									"Ha introducido una nota negativa que no se asignara al alumno, repita el proceso");
+						} else {
+							alumnos[i].setNotaMedia(notaMedia);
+						}
+						break;
+					}
+				}
+			}
+
+			case 4 -> {
+				System.out.println("Introduzca nombre y apellidos del alumno cuyos datos desea borrar: ");
+				nombre = dogma.nextLine();
+
+				for (int i = 0; i < alumnos.length; i++) {
+
+					if (alumnos[i].getNombre().equals(nombre)) {
+						alumnos[i].setNombre(null);
+						break;
+					}
+				}
+			}
+
+			case 5 -> {
+				// comunico cierre de programa con mensaje por pantalla
+				System.out.println("Cerrando programa...");
+			}
+
+			default -> {
+				// muestro mensaje de error por pantalla indicando opciones correctas
+				System.out.println("Asegurese de seleccionar una opcion entre el 1 y el 5 incluidos.");
+				System.out.println();
+			}
+
+			}
+
+		} while (seleccionUsuario != 5);
+		dogma.close();
 
 	}
 
