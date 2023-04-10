1ca974a594174a31d5d6da7f88da407d03ad6834
juanpc19 <juan.perez@iesnervion.es>
2023-04-10 10:18:24 +0200

    ej 4

--- Tema7EjerciciosCRUD/src/ejercicio4/Main.java
+++ Tema7EjerciciosCRUD/src/ejercicio4/Main.java
@@ -1,10 +1,27 @@
 package ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// CRUD opciones: crear cuenta, borrar cuenta, sacar dinero, ingresar dinero,
+		// mostrar informacion de cuenta
+		
+		String dni="";
+		
+		String nombre="";
+		
+		double saldo;
+		
+		Scanner dogma = new Scanner (System.in);
+		
+		CuentaCorriente cuenta []= new CuentaCorriente[100];
+		
+		
+
+		dogma.close();
 	}
 
 }
